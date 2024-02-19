cb1dcd96180388328f21c6ce3a4c6b61801ec0bf
John Winans <john@winans.org>
2024-02-18 20:41:42 -0600

    whitespace

--- fpga/prog/rom.asm
+++ fpga/prog/rom.asm
@@ -1,15 +1,15 @@
 ; This is loaded into what the CPU will see in address range 00000-001ff.
 
-	org 0
+        org 0
 
-	; The processor will run faster if it does not have to generate
-	; DRAM refresh and wait states that are enabled after reset.
+        ; The processor will run faster if it does not have to generate
+        ; DRAM refresh and wait states that are enabled after reset.
 
-	ld	a,0
-	out	(0x36),a	; RCR = 0 = disables the refresh controller,
-	out	(0x32),a	; DCNTL = 0 = zero wait states
+        ld      a,0
+        out     (0x36),a        ; RCR = 0 = disables the refresh controller
+        out     (0x32),a        ; DCNTL = 0 = zero wait states
 
-loop:	
-	jp	loop
+loop:   
+        jp      loop
 
-	end
+        end
