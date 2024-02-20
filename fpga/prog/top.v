2663eb83f2421cb9b91e08e520b3d998cec4306e
John Winans <john@winans.org>
2024-02-20 17:33:11 -0600

    Run SRAM async

--- fpga/prog/top.v
+++ fpga/prog/top.v
@@ -66,8 +66,9 @@ module top (
     wire [7:0]  rom_data;       // ROM output data bus
 
     // Instantiate the boot ROM
-    memory rom ( .rd_clk(hwclk), .addr(a[8:0]), .data(rom_data));
+    //memory rom ( .rd_clk(hwclk), .addr(a[8:0]), .data(rom_data));
     //memory rom ( .rd_clk(~rd_n), .addr(a[8:0]), .data(rom_data));
+    memory rom ( .rd_clk(rd_n), .addr(a[8:0]), .data(rom_data));
 
     assign reset_n = s1_n;      // route the reset signal to the CPU
 
@@ -93,7 +94,6 @@ module top (
     assign wait_n = 1'b1;       // de-assert /WAIT
 
     // Enable the static RAM on memory cycles to addresses >= 0x200.
-    // This should work OK only because the hwclk is 2+ times faster than PHI.
     assign ce_n = ~(~mreq_n && a >= 20'h200);
     assign oe_n = mreq_n | rd_n;
     assign we_n = mreq_n | wr_n;
