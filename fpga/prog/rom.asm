7f35763164f56274f50de607b2d4d9f61db06d1d
John Winans <john@winans.org>
2024-02-20 18:00:20 -0600

    Use OUT0 instructions

--- fpga/prog/rom.asm
+++ fpga/prog/rom.asm
@@ -1,7 +1,6 @@
 ; This is loaded into what the CPU will see in address range 00000-001ff.
 
-; The cheezy assembler I am using does not support the 180's OUT0 instruction.
-; BUT... if (and ONLY if) I am outputting a zero value, the result is the same.
+; NOTE: The cheezy assembler I am using does not support the 180's OUT0 instruction.
 
         org 0
 
@@ -9,10 +8,10 @@
         ; DRAM refresh and wait states that are enabled after reset.
 
         ld      a,0
-        //out0     (0x36),a        ; RCR = 0 = disables the refresh controller
-        db      0xdd,0x39,036   ; hand-made OUT0 instruction
-        //out0     (0x32),a        ; DCNTL = 0 = zero wait states
-        db      0xdd,0x39,0x32  ; hand-made OUT0 instruction
+        ;out0     (0x36),a      ; RCR = 0 = disables the refresh controller
+        db      0xed,0x39,0x36  ; hand-made OUT0 instruction
+        ;out0     (0x32),a      ; DCNTL = 0 = zero wait states
+        db      0xed,0x39,0x32  ; hand-made OUT0 instruction
 
 loop:   
         jp      loop
--- fpga/prog/top.v
+++ fpga/prog/top.v
@@ -66,9 +66,9 @@ module top (
     wire [7:0]  rom_data;       // ROM output data bus
 
     // Instantiate the boot ROM
-    //memory rom ( .rd_clk(hwclk), .addr(a[8:0]), .data(rom_data));
+    memory rom ( .rd_clk(hwclk), .addr(a[8:0]), .data(rom_data));
     //memory rom ( .rd_clk(~rd_n), .addr(a[8:0]), .data(rom_data));
-    memory rom ( .rd_clk(rd_n), .addr(a[8:0]), .data(rom_data));
+    //memory rom ( .rd_clk(rd_n), .addr(a[8:0]), .data(rom_data));
 
     assign reset_n = s1_n;      // route the reset signal to the CPU
 
