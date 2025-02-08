6c20e3de77226ac3cfaecf33b3bf6c50686d0535
John Winans <john@winans.org>
2025-02-08 11:44:25 -0600

    Cleanup test case with sane timing values

--- fpga/lib/cdc/z80_wr_cdc_tb.v
+++ fpga/lib/cdc/z80_wr_cdc_tb.v
@@ -19,20 +19,25 @@
 //
 //**************************************************************************
 
-`timescale 1ns/10ps
+`timescale 1ns/1ns
 `default_nettype none
 
 module tb ();
 
-    // the Z8S180 interface signals of interest
-    reg reset;
-    reg iorq;
-    reg wr;
-    reg [7:0] din1;
-    reg [7:0] ain1;
+    // The Z8S180 interface signals of interest.
+    // Initialize here before any initial or always blocks start.
+    reg reset       = 0;
+    reg iorq        = 0;
+    reg wr          = 0;
+    reg [7:0] din1  = 'hz;
+    reg [7:0] ain1  = 'hz;
 
-    reg clk1;           // the PHI clock 18.432MHZ
-    reg clk2;           // the pixel clock 25MHZ
+    reg phi         = 0;    // the PHI clock 18.432MHZ
+    reg pxclk       = 0;    // the pixel clock 25MHZ
+
+    reg t1_marker   = 0;    // used to make obvious which are T1 phi clocks
+
+
 
     wire wr_tick1;
     wire wr_tick2;
@@ -42,7 +47,7 @@ module tb ();
     // generate a write tick in the PHI domain
     iorq_wr_fsm wr_fsm (
         .reset(reset), 
-        .phi(clk1), 
+        .phi(phi), 
         .iorq(iorq), 
         .wr(wr), 
         .wr_tick(wr_tick1) 
@@ -51,8 +56,8 @@ module tb ();
     // generate a write tick in the VDP domain
     z80_wr_cdc wr_cdc (
         .reset(reset),
-        .clk1(clk1),
-        .clk2(clk2),
+        .clk1(phi),
+        .clk2(pxclk),
         .wr_tick1(wr_tick1),
         .din1(din1),
         .ain1(ain1),
@@ -61,99 +66,109 @@ module tb ();
         .aout2(aout2)
     );
 
-    localparam clk1_period = (1.0/18432000)*1000000000; // clk1 is running at about 18.432MHZ
-    localparam clk2_period = (1.0/25000000)*1000000000; // clk2 is running at 25MHZ
+    localparam phi_period   = (1.0/18432000)*1000000000;    // phi is running at about 18.432MHZ
+    localparam pxclk_period = (1.0/25000000)*1000000000;    // pxclk is running at 25MHZ
 
-    always #(clk1_period) clk1 = ~clk1;
-    always #(clk2_period) clk2 = ~clk2;
+    // generate free-running clocks
+    always #(phi_period/2) phi = ~phi;
+    always #(pxclk_period/2) pxclk = ~pxclk;
 
     initial begin
         $dumpfile("z80_wr_cdc_tb.vcd");
         $dumpvars;
 
-        reset = 1;
-        clk1 = 0;
-        clk2 = 0;
-        iorq = 0;
-        wr = 0;
-        din1 = 'hz;
-        ain1 = 'hz;
-
-        #(clk1_period*2);
+        reset <= 1;
+        #(phi_period*4);
         reset <= 0;
-        #(clk1_period*2);;
+        #(phi_period*2);;
 
-        // This allows us to wait until the next clk1 rising edge (simulation only)
-        @(posedge clk1);        // this as T1 of an IORQ write cycle
 
-        // A realistic 4-T Z8S180 IO write transaction
-        #5;
-        ain1 <= 8'h81;          // address valid by 5ns after T1 rising edge
+        // A realistic moderately timed 4-T Z8S180 IO write transaction
 
-        @(negedge clk1);
-        #15;                    // data valid max 25ns after T1 falling and 10ns before T2 rising
-        din1 <= 8'h23;          // some data value to write to the IO port
+        // This allows us to wait until the next phi rising edge (simulation only)
+        @(posedge phi);         // T1 rising
+        t1_marker <= 1;
+        t1_marker <= #(phi_period) 0;
 
-        // iorq = 25ns after T1 falling worst case
-        // rd = 25ns after T1 falling worst case
-        #5; // meh
-        iorq <= 1;
+        ain1 <= #12 8'h81;      // address valid after T1 rising and >5ns before IORQ
 
-        @(posedge clk1);        // wait for T2 rising edge
-        #22;                    // 25ns worst case
-        wr <= 1;
+        @(negedge phi);         // T1 falling
+        din1 <= #5 8'h23;       // data <25ns after T1 falling and >10ns before WR active
+        iorq <= #15 1;          // iorq = <25ns after T1 falling 
+                                // rd = <25ns after T1 falling 
+        @(posedge phi);         // T2 rising
+        wr <= #12 1;            // <25ns after T2 rising
 
-        @(posedge clk1);        // Wait for Tw rising
-        @(posedge clk1);        // Wait for T3 rising
-        @(negedge clk1);        // Wait for T3 falling
-        #20;                    // 25ns worst case
-        wr <= 0;
-        iorq <= 0;
-        
-        @(posedge clk1);        // wait for T1 rising of next bus cycle
-        din1 = 'hz;
-        ain1 = 'hz;
+        @(posedge phi);         // Tw rising
+        @(posedge phi);         // T3 rising
+        @(negedge phi);         // T3 falling
 
-        
-        // skip the opcode fetch bus cycle
-        @(posedge clk1);        // T2
-        @(posedge clk1);        // T3
+        iorq <= #14 0;          // iorq <25ns after T3 falling
+        wr <= #12 0;            // wr <25ns after T3 falling
+                                // rd <25ns after T3 falling
+
+        @(negedge wr)           // be careful that wr does not happen after posedge phi here!!!
+        ain1 <= #6 'hz;         // >5ns after iorq & wr trailing
+        din1 <= #12 'hz;        // >10ns after wr trailing
+
+        @(posedge phi);         // wait for T1 rising of next bus cycle (opcode fetch)
+        t1_marker <= 1;
+        t1_marker <= #(phi_period) 0;
+        @(posedge phi);         // T2 opcode fetch
+        @(posedge phi);         // T3 opcode fetch
 
         // skip the operand fetch as if this were an OUT (nn),A instruction
-        @(posedge clk1);        // T1
-        @(posedge clk1);        // T2
-        @(posedge clk1);        // T3
-
-        // The IORQ WR cycle
-        #5;
-        ain1 <= 8'h81;          // address valid by 5ns after T1 rising edge
-
-        @(negedge clk1);
-        #15;                    // data valid max 25ns after T1 falling and 10ns before T2 rising
-        din1 <= 8'h82;          // write to VDP register 2
-
-        // iorq = 25ns after T1 falling worst case
-        // rd = 25ns after T1 falling worst case
-        #5; // meh
-        iorq <= 1;
-
-        @(posedge clk1);        // wait for T2 rising edge
-        #22;                    // 25ns worst case
-        wr <= 1;
-
-        @(posedge clk1);        // Wait for Tw rising
-        @(posedge clk1);        // Wait for T3 rising
-        @(negedge clk1);        // Wait for T3 falling
-        #20;                    // 25ns worst case
-        wr <= 0;
-        iorq <= 0;
-        
-        @(posedge clk1);        // wait for T1 rising of next bus cycle
-        din1 = 'hz;
-        ain1 = 'hz;
+        @(posedge phi);         // wait for T1 rising of next bus cycle (operand fetch)
+        t1_marker <= 1;
+        t1_marker <= #(phi_period) 0;
+        @(posedge phi);         // T2 operand fetch
+        @(posedge phi);         // T3 operand fetch
 
 
-        #(clk1_period*10);;
+
+
+        // A worst case timed 4-T Z8S180 IO write transaction
+
+        @(posedge phi);         // T1 IO cycle starts
+        t1_marker <= 1;
+        t1_marker <= #(phi_period) 0;
+
+        @(negedge phi);         // T1 falling
+
+        ain1 <= #20 8'h81;      // address valid >5ns before IORQ
+        din1 <= #25 8'h23;      // data <25ns after T1 falling and >10ns before WR active
+
+        // rd = <25ns after T1 falling 
+        iorq <= #25 1;          // iorq = <25ns after T1 falling
+
+        @(posedge phi);         // T2 rising
+        wr <= #25 1;            // wr <25ns after T2 raising
+
+        @(posedge phi);         // Tw rising
+        @(posedge phi);         // T3 rising
+        @(negedge phi);         // T3 falling
+
+        iorq <= #1 0;           // iorq <25ns after T3 falling
+        wr <= #1 0;             // wr <25ns after T3 falling
+                                // rd <25ns after T3 falling
+
+        @(negedge wr)           // be careful that wr does not happen after posedge phi here!!!
+        ain1 <= #6 'hz;         // >5ns after iorq & wr trailing
+        din1 <= #12 'hz;        // >10ns after wr trailing
+
+
+
+
+        // skip the next opcode fetch bus cycle
+        @(posedge phi);         // wait for T1 rising of next bus cycle
+        t1_marker <= 1;
+        t1_marker <= #(phi_period) 0;
+        @(posedge phi);         // T2 opcode fetch
+        @(posedge phi);         // T3 opcode fetch
+
+
+        // waste some time and end it
+        #(phi_period*3);
         $finish;
     end
  
