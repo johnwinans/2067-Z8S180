2b3df2431df2d84a28dd276e0e9b2dd338beb142
John Winans <john@winans.org>
2025-02-19 18:39:54 -0600

    Reset VDP regs on... reset

--- fpga/vdp99/vdp_reg_ifce_tb.v
+++ fpga/vdp99/vdp_reg_ifce_tb.v
@@ -23,11 +23,11 @@
 
 module tb();
 
-    reg clk;        // pixel clock
-    reg reset;
-    reg wr_tick;
-    reg rd_tick;
-    reg [7:0] din;
+    reg clk         = 0;        // pixel clock
+    reg reset       = 0;
+    reg wr_tick     = 0;
+    reg rd_tick     = 0;
+    reg [7:0] din   = 'hx;
 
     vdp_reg_ifce uut (
         .clk(clk),
@@ -37,129 +37,270 @@ module tb();
         .din(din)
     );
 
+`define ASSERT(cond) if ( !(cond) ) $display("%s:%0d %m time:%3t ASSERTION (cond) FAILED!", `__FILE__, `__LINE__, $time );
+
     initial begin
         $dumpfile("vdp_reg_ifce_tb.vcd");
         $dumpvars;
     end
     
-    always #1 clk = ~clk;
+    localparam clk_period = (1.0/25000000)*1000000000; // clk is running at 25MHZ
+    always #(clk_period/2) clk = ~clk;
 
     initial begin
-        clk = 0;
-        wr_tick = 0;
-        rd_tick = 0;
-        din = 'hz;
-        reset = 1;
-        #5;
+        #(clk_period*4);
 
+        @(posedge clk);
+        reset <= 1;
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
         reset <= 0;
-        #4;
 
-        //#0.3;           // exaggerate the tick changes after the rising edges
+
+        @(posedge clk);
+
+        `ASSERT( uut.r0 === 0 );
+        `ASSERT( uut.r1 === 0 );
+        `ASSERT( uut.r2 === 0 );
+        `ASSERT( uut.r3 === 0 );
+        `ASSERT( uut.r4 === 0 );
+        `ASSERT( uut.r5 === 0 );
+        `ASSERT( uut.r6 === 0 );
+        `ASSERT( uut.r7 === 0 );
+
+        @(posedge clk);
+        @(posedge clk);
 
         // reg0 = 0xee
+        @(posedge clk);
         din <= 'hee;
         wr_tick <= 1;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.w0_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.r0 === 0 );
+
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 1 );
+        `ASSERT( uut.w0_reg === 'hee );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.r0 === 0 );
+
+        @(posedge clk);
         din <= 'h80;     // write into reg 0
         wr_tick <= 1;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 1 );
+        `ASSERT( uut.w0_reg === 'hee );
+        `ASSERT( uut.update_vdp_reg_tick === 1 );
+        `ASSERT( uut.r0 === 0 );
+
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.w0_reg === 'hee );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.r0 === 'hee );
+
+
 
         // reg3 = 0x33
+        @(posedge clk);
         din <= 'h33;
         wr_tick <= 1;
-        #2;
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+
+        @(posedge clk);
         din <= 'h83;     // write into reg 3
         wr_tick <= 1;
-        #2;
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h33 );
+        `ASSERT( uut.r0 === 'hee);
+        `ASSERT( uut.r1 === 'h00);
+        `ASSERT( uut.r2 === 'h00);
+        `ASSERT( uut.r3 === 'h33);
+        `ASSERT( uut.r4 === 'h00);
+        `ASSERT( uut.r5 === 'h00);
+        `ASSERT( uut.r6 === 'h00);
+        `ASSERT( uut.r7 === 'h00);
+
 
         // rapid-fire writes
+        @(posedge clk);
         din <= 'h44;
         wr_tick <= 1;
-        #2;
+        @(posedge clk);
         din <= 'h84;
-        #2;
+        @(posedge clk);
+        @(negedge clk);
+        `ASSERT( uut.r4 === 'h44 );
+
         din <= 'h55;
-        #2;
+        @(posedge clk);
         din <= 'h85;
-        #2;
+        @(posedge clk);
+        @(negedge clk);
+        `ASSERT( uut.r5 === 'h55 );
         din <= 'h66;
-        #2;
+        @(posedge clk);
         din <= 'h86;
-        #2;
+        @(posedge clk);
+        @(negedge clk);
+        `ASSERT( uut.r6 === 'h66 );
         wr_tick <= 0;
         din <= 'hz;
-        #4;
+
+
+        @(posedge clk);
+        @(posedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h66 );
+        `ASSERT( uut.r0 === 'hee);
+        `ASSERT( uut.r1 === 'h00);
+        `ASSERT( uut.r2 === 'h00);
+        `ASSERT( uut.r3 === 'h33);
+        `ASSERT( uut.r4 === 'h44);
+        `ASSERT( uut.r5 === 'h55);
+        `ASSERT( uut.r6 === 'h66);
+        `ASSERT( uut.r7 === 'h00);
+
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
 
         // test rm0 reset the 2-state write register toggler
+        @(posedge clk);
         din <= 'h22;
         wr_tick <= 1;
-        #2;
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 1 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h22 );
+        `ASSERT( uut.r1 === 'h00);
 
+
+        @(posedge clk);
         rd_tick <= 1;
-        #2;
+        @(posedge clk);
         rd_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h22 );
+        `ASSERT( uut.r1 === 'h00);
+
 
         // make sure it still works (we didn't get it stuck)
+        @(posedge clk);
         din <= 'h11;
         wr_tick <= 1;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h22 );
+        `ASSERT( uut.r1 === 'h00);
+
+        @(posedge clk);
         din <= 'h81;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 1 );
+        `ASSERT( uut.update_vdp_reg_tick === 1 );
+        `ASSERT( uut.w0_reg === 'h11 );
+        `ASSERT( uut.r1 === 'h00);
+
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h11 );
+        `ASSERT( uut.r1 === 'h11);
+
 
         // make sure we can CHANGE a value that has already been written
+        @(posedge clk);
         din <= 'hf6;
         wr_tick <= 1;
-        #2;
+        @(posedge clk);
         din <= 'h86;
-        #2;
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'hf6 );
+        `ASSERT( uut.r6 === 'hf6);
 
         // make sure that rm0 is OK when not done in state 1
+        @(posedge clk);
         rd_tick <= 1;
-        #2;
+        @(posedge clk);
         rd_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'hf6 );
+        `ASSERT( uut.r6 === 'hf6);
 
         // a long wait between successive wm0 writes
+        @(posedge clk);
         din <= 'h77;
         wr_tick <= 1;
-        #2;
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #6;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 1 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h77 );
+        `ASSERT( uut.r7 === 'h00);
+
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
         wr_tick <= 1;
         din <= 'h87;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 1 );
+        `ASSERT( uut.update_vdp_reg_tick === 1 );
+        `ASSERT( uut.w0_reg === 'h77 );
+        `ASSERT( uut.r7 === 'h00);
+
+        @(posedge clk);
         wr_tick <= 0;
         din <= 'hz;
-        #2;
+        @(negedge clk);
+        `ASSERT( uut.state_reg === 0 );
+        `ASSERT( uut.update_vdp_reg_tick === 0 );
+        `ASSERT( uut.w0_reg === 'h77 );
+        `ASSERT( uut.r7 === 'h77);
 
+        @(posedge clk);
 
-        #20;
+        #(clk_period*10);
         $finish;
     end
 
