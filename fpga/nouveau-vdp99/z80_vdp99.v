255161dec806e5ee5454328f60bd191912f35e41
John Winans <john@winans.org>
2025-08-16 12:50:28 -0500

    timescale

--- fpga/nouveau-vdp99/z80_vdp99.v
+++ fpga/nouveau-vdp99/z80_vdp99.v
@@ -19,9 +19,9 @@
 //
 //**************************************************************************
 
-
 // The purpose of this module is to connect the CPU clock domain to the VDP clock domain.
 
+`timescale 1ns/1ns
 `default_nettype none
 
 module z80_vdp99 #(
